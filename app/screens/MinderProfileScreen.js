c42a443d50dedbff844d26052be6f590e293972d
James Budge <thebudgie@gmail.com>
2021-04-26 12:46:01 +0100

    Adds About Card

--- app/screens/MinderProfileScreen.js
+++ app/screens/MinderProfileScreen.js
@@ -5,22 +5,33 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  View,
 } from "react-native";
-import minderProfile from "../data/minderProfile";
+import { minder } from "../data/minderProfile";
 
 function MinderProfileScreen(props) {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text style={styles.topText}>ᗕ Minder Profile</Text>
-        <Image
-          style={styles.minder}
-          source={minderProfile.minder.profileURL}
-        ></Image>
-        <Image
-          style={styles.minderCard}
-          source={require("../assets/images/cardbase.png")}
-        ></Image>
+        <View>
+          <Text style={styles.topText}>ᗕ Minder Profile</Text>
+        </View>
+        <View>
+          <Image style={styles.minder} source={minder.profileURL}></Image>
+        </View>
+        <View>
+          <Image
+            style={styles.minderCard}
+            source={require("../assets/images/cardbase.png")}
+          ></Image>
+        </View>
+        <View>
+          <Text style={styles.aboutText}>About {minder.firstName}</Text>
+          <Image source={require("../assets/images/divider.png")}></Image>
+          <Text>{minder.about}</Text>
+          <Image source={require("../assets/images/divider.png")}></Image>
+          <Text>Read More</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -28,10 +39,11 @@ function MinderProfileScreen(props) {
 
 const styles = StyleSheet.create({
   topText: {
+    flex: 1,
     fontSize: 20,
     left: "5%",
-    top: 10,
-    height: "10%",
+    alignContent: "center",
+    padding: 15,
   },
   minder: {
     flex: 1,
@@ -46,6 +58,23 @@ const styles = StyleSheet.create({
     width: "90%",
     borderRadius: 20,
   },
+  aboutText: {
+    flex: 1,
+    flexDirection: "column",
+    // top: -50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
+
+const PurpleCircle = () => (
+  <View
+    style={{
+      width: 10,
+      height: 10,
+      backgroundColor: purple,
+    }}
+  />
+);
 
 export default MinderProfileScreen;
